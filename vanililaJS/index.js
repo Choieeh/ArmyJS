5291e09da482d8a26fdca10b53332169c33b3b39
Choieeh <chyh0182@naver.com>
2020-07-14 20:55:49 +0900

    Create index.js

---
+++ vanililaJS/index.js
@@ -0,0 +1,8 @@
+function sayHello(name, age) {
+  console.log(`Hello ${name} you are ${age} years old`);
+}
+
+const greetNicolas = sayHello("Nicolas",15);
+
+console.log(greetNicolas)
+//아래꺼는 undefined뜸. return값이 없기때문
